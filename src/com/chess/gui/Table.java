cbe18975accce972a0833ddcbc45085eaa9bebca
hakroala <hakroala@bu.edu>
2020-12-30 14:36:10 -0500

    create UI for Board and TileColor

--- src/com/chess/gui/Table.java
+++ src/com/chess/gui/Table.java
@@ -1,19 +1,117 @@
 package com.chess.gui;
 
+import com.chess.engine.board.BoardUtils;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Table
 {
     private final JFrame gameFrame;
+    private final BoardPanel boardPanel;
 
-    private static Dimension OUTER_FRAME_DIMENSION = new Dimension(600,600);
+    private final static Dimension OUTER_FRAME_DIMENSION = new Dimension(600,600);
+    private final static Dimension BOARD_PANEL_DIMENSION = new Dimension(400,350);
+    private final static Dimension TILE_PANEL_DIMENSION = new Dimension(10,10);
     public Table()
     {
         this.gameFrame = new JFrame("Jchess");
-
+        this.gameFrame.setLayout(new BorderLayout());
+        final JMenuBar tableMenuBar = createTableMenuBar();
+        this.gameFrame.setJMenuBar(tableMenuBar);
         this.gameFrame.setSize((OUTER_FRAME_DIMENSION));
+        this.boardPanel = new BoardPanel();
+        this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
         this.gameFrame.setVisible(true);
 
     }
+
+    private JMenuBar createTableMenuBar()
+    {
+        final JMenuBar tableMenuBar = new JMenuBar();
+        tableMenuBar.add(createFileMenu());
+        return tableMenuBar;
+    }
+
+    private JMenu createFileMenu()
+    {
+        final JMenu fileMenu = new JMenu("File");
+
+        final JMenuItem openPGN = new JMenu("Load PGN file");
+
+        openPGN.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Open up that PGN file");
+            }
+        });
+
+        fileMenu.add(openPGN);
+        final JMenuItem exitMenuItem = new JMenu("Exit");
+        exitMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        fileMenu.add(exitMenuItem);
+        return fileMenu;
+    }
+
+    private class BoardPanel extends JPanel
+    {
+        final List<TilePanel> boardTiles;
+
+        BoardPanel(){
+            super(new GridLayout(8,8));
+            this.boardTiles = new ArrayList<>();
+            for (int i = 0; i < BoardUtils.NUM_TILES; i++)
+            {
+                final TilePanel tilePanel = new TilePanel(this,i);
+                this.boardTiles.add(tilePanel);
+                add(tilePanel);
+            }
+            setPreferredSize(BOARD_PANEL_DIMENSION);
+            validate();
+        }
+    }
+
+    private class TilePanel extends JPanel
+    {
+        private final int tileId;
+
+        TilePanel(final  BoardPanel boardPanel,
+                  final int tileId)
+        {
+            super(new GridLayout());
+            this.tileId = tileId;
+            setPreferredSize(TILE_PANEL_DIMENSION);
+            assignTileColor();
+            validate();
+        }
+        private void assignTileColor()
+        {
+            if(BoardUtils.FIRST_ROW[this.tileId] ||
+                BoardUtils.THIRD_ROW[this.tileId]||
+                BoardUtils.SEVENTH_ROW[this.tileId] ||
+                BoardUtils.FIFTH_ROW[this.tileId])
+            {
+                setBackground(this.tileId % 2 == 0 ? lightTileColor : darkTileColor);
+            }
+            else if (BoardUtils.SECOND_ROW[this.tileId] ||
+                    BoardUtils.FOURTH_ROW[this.tileId]||
+                    BoardUtils.SIXTH_ROW[this.tileId] ||
+                    BoardUtils.EIGHTH_ROW[this.tileId])
+            {
+                setBackground(this.tileId % 2 != 0 ? lightTileColor : darkTileColor);
+            }
+        }
+        }
+    }
+
 }
